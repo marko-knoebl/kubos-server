8fc996092b063109cd3f7d6c37b0dc67c60a108f
Marko Knöbl <marko.kn@gmail.com>
2014-10-01 10:51:55 +0000

    initial commit

---
+++ kubos_pythonanywhere_com_wsgi.py
@@ -0,0 +1,28 @@
+# This file contains the WSGI configuration required to serve up your
+# web application at http://kubos.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+
+import webapp2
+#webapp2.application
+
+from server import kubos
+from server import static_file_handler
+
+routes = [
+    ('/', kubos.KubosBoxesApp),
+    ('/kubos', kubos.KubosApp),
+    ('/solids', kubos.KubosApp),
+    ('/boxes', kubos.KubosBoxesApp),
+    ('/blocks', kubos.KubosBoxesApp),
+    ('/kubos_boxes', kubos.KubosBoxesApp),
+    ('/upload_stl', kubos.UploadStl),
+    ('/(.+)', static_file_handler.StaticFileHandler),
+]
+
+config = {}
+config['webapp2_extras.sessions'] = {
+    'secret_key': 'something-very-very-secret',
+}
+
+application = webapp2.WSGIApplication(routes=routes, debug=True, config=config)
